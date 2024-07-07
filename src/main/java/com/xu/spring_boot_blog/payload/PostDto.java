f47d5bb69fc344404e3f07c70d2c7d5b2a1faed7
xu <godglasses.xu@gmail.com>
2024-07-07 11:15:56 +0800

    update PostDto set CommentDto

--- src/main/java/com/xu/spring_boot_blog/payload/PostDto.java
+++ src/main/java/com/xu/spring_boot_blog/payload/PostDto.java
@@ -2,12 +2,15 @@ package com.xu.spring_boot_blog.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 
 
 }
