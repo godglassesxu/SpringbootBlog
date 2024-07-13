d060e4d6da028c95cb4273b574f74d0cb6324d12
xu <godglasses.xu@gmail.com>
2024-07-13 23:24:35 +0800

    add validation

--- src/main/java/com/xu/spring_boot_blog/payload/PostDto.java
+++ src/main/java/com/xu/spring_boot_blog/payload/PostDto.java
@@ -1,14 +1,23 @@
 package com.xu.spring_boot_blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 import java.util.Set;
 
 @Data
 public class PostDto {
     private long id;
+
+    @NotEmpty
+    @Size(min = 1, message = "標題字少要一個字")
     private String title;
+
+    @NotEmpty
+    @Size(min = 1, message = "請輸入至少一個字")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 
