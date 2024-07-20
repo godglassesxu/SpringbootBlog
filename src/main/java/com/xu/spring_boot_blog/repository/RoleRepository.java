f58eb52e6d3557780aec6c90e64d9b4010bbb77d
xu <godglasses.xu@gmail.com>
2024-07-20 14:49:52 +0800

    add User & Role

---
+++ src/main/java/com/xu/spring_boot_blog/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.xu.spring_boot_blog.repository;
+
+import com.xu.spring_boot_blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
