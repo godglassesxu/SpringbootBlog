fe27d6405edae3d0f2ce81acd2396ce223cd83c3
xu <godglasses.xu@gmail.com>
2024-07-20 19:23:45 +0800

    由memory改使用DB進用使用者驗證

--- src/main/java/com/xu/spring_boot_blog/config/SecurityConfig.java
+++ src/main/java/com/xu/spring_boot_blog/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.xu.spring_boot_blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +20,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -37,21 +50,22 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails  gust = User.builder()
-                .username("gust")
-                .password(passwordEncoder().encode("gust"))
-                .roles("USER")
-                .build();
-
-        UserDetails  admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return  new InMemoryUserDetailsManager(gust, admin);
-    }
+// 改用AuthenticationManager
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails  gust = User.builder()
+//                .username("gust")
+//                .password(passwordEncoder().encode("gust"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails  admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return  new InMemoryUserDetailsManager(gust, admin);
+//    }
 
 }
